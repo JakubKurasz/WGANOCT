e15430ef151b481043ebef529afcfc73530c0d85
JakubJKurasz <69475495+JakubKurasz@users.noreply.github.com>
2025-03-11 15:37:11 +0000

    Add run file

---
+++ Run.py
@@ -0,0 +1,96 @@
+
+from PreProccesing import preProcessing
+from Model import Generator
+from Model import Discriminator
+from Model import gradient_penalty
+from Model import initialize_weights
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+
+if __name__ == "__main__":
+    # Hyperparameters etc.
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    LEARNING_RATE = 1e-4
+    BATCH_SIZE = 32
+    CHANNELS_IMG = 1
+    Z_DIM = 100
+    NUM_EPOCHS = 500
+    FEATURES_CRITIC = 16
+    FEATURES_GEN = 16
+    CRITIC_ITERATIONS = 5
+    LAMBDA_GP = 10
+
+
+    train_dl, test_dl = preProcessing()
+
+    # initialize gen and disc, note: discriminator should be called critic,
+    # according to WGAN paper (since it no longer outputs between [0, 1])
+    gen = Generator(Z_DIM, CHANNELS_IMG, FEATURES_GEN).to(device)
+    critic = Discriminator(CHANNELS_IMG, FEATURES_CRITIC).to(device)
+    initialize_weights(gen)
+    initialize_weights(critic)
+
+    fixed_noise = torch.randn(32, Z_DIM, 1, 1).to(device)
+
+    # initializate optimizer
+    opt_gen = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.0, 0.9))
+    opt_critic = optim.Adam(critic.parameters(), lr=LEARNING_RATE, betas=(0.0, 0.9))
+
+
+    step = 0
+
+    gen.train()
+    critic.train()
+
+    for epoch in range(NUM_EPOCHS):
+        # Target labels not needed! <3 unsupervised
+        for batch_idx, (real, _) in enumerate(train_dl):
+            real = real.to(device)
+            cur_batch_size = real.shape[0]
+            # Train Critic: max E[critic(real)] - E[critic(fake)]
+            # equivalent to minimizing the negative of that
+            for _ in range(CRITIC_ITERATIONS):
+                noise = torch.randn(cur_batch_size, Z_DIM, 1, 1).to(device)
+                fake = gen(noise)
+                critic_real = critic(real).reshape(-1)
+                critic_fake = critic(fake).reshape(-1)
+                gp = gradient_penalty(critic, real, fake, device=device)
+                loss_critic = (
+                    -(torch.mean(critic_real) - torch.mean(critic_fake)) + LAMBDA_GP * gp
+                )
+                critic.zero_grad()
+                loss_critic.backward(retain_graph=True)
+                opt_critic.step()
+
+            # Train Generator: max E[critic(gen_fake)] <-> min -E[critic(gen_fake)]
+
+            gen_fake = critic(fake).reshape(-1)
+            loss_gen = -torch.mean(gen_fake)
+            gen.zero_grad()
+            loss_gen.backward()
+            opt_gen.step()
+
+            # Print losses occasionally and print to tensorboard
+            if batch_idx % 44 == 0 and batch_idx > 0:
+                print(
+                    f"Epoch [{epoch}/{NUM_EPOCHS}] Batch {batch_idx+1}/{len(train_dl)} \
+                    Loss D: {loss_critic:.4f}, loss G: {loss_gen:.4f}"
+                )
+
+                with torch.no_grad():
+                    fake = gen(fixed_noise)
+                    # take out (up to) 32 examples
+
+                    img_grid_fake = torchvision.utils.make_grid(fake[:32], normalize=True)
+
+                    output_path = "fake_image.png"
+                    torchvision.utils.save_image(img_grid_fake, output_path)
+
+                step += 1
